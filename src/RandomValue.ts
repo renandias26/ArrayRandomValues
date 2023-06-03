a8b8b808cdf721e96e4344073fd979d59c5be7cd
Renan Dias <renan.dias@obraprimaweb.com.br>
2023-06-03 12:28:35 -0300

    Add method getRandomValue

--- src/RandomValue.ts
+++ src/RandomValue.ts
@@ -7,5 +7,19 @@ class RandomValue{
         this.Array = Array
     }
 
-    
+    getRandomValue(Quantity: number): Products[]{
+        if(this.Array.length < Quantity) return new Array<Products>
+
+        let RandomValues: Products[] = new Array<Products>
+
+        while(this.Array.length > Quantity){
+            const RandomIndex:number = Math.floor(Math.random() * this.Array.length)
+
+            if(!RandomValues.includes(this.Array[RandomIndex])){
+                RandomValues.push(this.Array[RandomIndex])
+            }
+        }
+
+        return RandomValues
+    }
 }
